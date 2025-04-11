b170d1b20a488a82acebe60c36ba9762140351e0
theo.borella.hc@gmail.com <theo.borella.hc@gmail.com>
2025-04-11 09:59:14 +0200

    (feat) : Npm run build

--- hackaton_front_react/vite.config.js
+++ hackaton_front_react/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -10,20 +11,16 @@ export default defineConfig({
       manifest: {
         name: 'BienSitué',
         short_name: 'BienSitué',
-        description: 'Votre app immobilière intelligente à Lyon',
-        start_url: '/',
-        scope: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#c8924f',
+        description: 'Votre guide immobilier à Lyon',
+        theme_color: '#ffffff',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
@@ -31,4 +28,4 @@ export default defineConfig({
       }
     })
   ]
-});
+})
